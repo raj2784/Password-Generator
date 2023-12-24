16aa11e72a4caae83e09984655c1fdc5bc83cb56
Raj Bhatia <raj2784@hotmail.com>
2023-12-24 15:20:18 +1030

    React Password generator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useCallback, useEffect, useRef } from "react";
 
 function App() {
+  const [length, setLength] = useState(7);
+  const [numAllowed, setNumAllowed] = useState(false);
+  const [specialCharAllowed, setSpecialCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+  //useRef Hook
+  const passwordRef = useRef(null);
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijkhmnopqrstuvwxyz";
+    if (numAllowed) str += "0123456789";
+    if (specialCharAllowed) str += "~!@#$%^&*()_+`[]{}";
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+    setPassword(pass);
+  }, [length, numAllowed, specialCharAllowed, setPassword]);
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numAllowed, specialCharAllowed, passwordGenerator]);
+
+  const copyPasswordToClipboard = useCallback(() => {
+    //showing on UI on copy to clipboard select whole text
+    passwordRef.current?.select();
+    //optionally pass the range you wanted to select
+    //passwordRef.current?.setSelectionRange(0, 7);
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-700 bg-gray-700 text mt-40">
+        <h1 className="text-white text-center my-3">
+          React Password Generator
+        </h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="Passowrd"
+            ref={passwordRef}
+            readOnly
+          />
+          <button
+            className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0 hover:bg-red-700"
+            onClick={copyPasswordToClipboard}
+          >
+            copy
+          </button>
+        </div>
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-2">
+            <input
+              type="range"
+              min={7}
+              max={100}
+              value={length}
+              className="cursor-pointer"
+              onChange={(e) => {
+                setLength(e.target.value);
+              }}
+            />
+            <label htmlFor="length">Length : ({length})</label>
+          </div>
+          <div className="flex items-center gap-x-2">
+            <input
+              type="checkbox"
+              defaultChecked={numAllowed}
+              id="numberInput"
+              onChange={() => {
+                setNumAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="numInput">Numbers {numAllowed}</label>
+          </div>
+          <div className="flex items-center gap-x-2">
+            <input
+              type="checkbox"
+              defaultChecked={specialCharAllowed}
+              id="charInput"
+              onChange={() => {
+                setSpecialCharAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="charInput">Special Char {specialCharAllowed}</label>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
